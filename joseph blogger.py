08ecb83eea665552b5bb378c8ed1c0f244f2e2ee
xXBillClintonXx <80366500+xXBillClintonXx@users.noreply.github.com>
2021-05-06 14:37:03 +1200

    Add files via upload

---
+++ joseph blogger.py
@@ -0,0 +1,4 @@
+print ("Tyler" + " Lyons Peters")
+name = input ("What is your name?")
+age = input ("How old are you?")
+print (" Don't tell strangers you name and age, {}. Unwise for a {} year old like you." .format (name, age))
