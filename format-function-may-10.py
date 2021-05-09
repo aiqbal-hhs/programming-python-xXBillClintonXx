fc61f4b10fc3d155f7d4759091aaa5f78449a1d9
xXBillClintonXx <80366500+xXBillClintonXx@users.noreply.github.com>
2021-05-10 10:55:06 +1200

    Add files via upload

---
+++ format-function-may-10.py
@@ -0,0 +1,7 @@
+name = input("What is their name?")
+age = input("How old are they?")
+job = input ("What do they do?")
+height= input ("How tall are they?")
+networth = input("What is their net worth?")
+
+print("According to you {} is  a {} year old {} who is {} metres tall and has a net worth of {} dollars".format(name, age, job, height, networth))
